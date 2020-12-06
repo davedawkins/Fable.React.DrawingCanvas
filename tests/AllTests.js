2e701cbab4d5e11e5641231eb5c1c05841fb5f65
David Dawkins <david.dawkins@gmail.com>
2020-12-06 20:44:49 +0000

    Updates

--- tests/AllTests.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _TestDrawingCanvas = require("./Test.DrawingCanvas.Builder");
-
-var _TestDrawingCanvas2 = require("./Test.DrawingCanvas.Turtle");
-
-var _Mocha = require("./Fable.Mocha.1.0.0/Mocha");
-
-(0, _Mocha.Mocha$$$runTests)([_TestDrawingCanvas.tests, _TestDrawingCanvas2.tests]);
