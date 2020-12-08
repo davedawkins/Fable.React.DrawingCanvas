04ea4ea737b77e5014e9fb30764758adfca210e3
David Dawkins <david.dawkins@gmail.com>
2020-12-08 22:53:33 +0000

    Color animation

--- demos/Turtle/Turtle.fs
+++ demos/Turtle/Turtle.fs
@@ -8,20 +8,48 @@ open Fable.React.DrawingCanvas
 open Fable.React.DrawingCanvas.Turtle
 open Fulma
 open SelectForCustomTypes
+open Browser.Dom
 
-type Model = {
-    Color: string
-}
+type TickId = float option
+
+type Model = { Color: string; Tick: TickId }
 
 type Msg =
     | SetColor of string
+    | Tick
+    | SetTickId of TickId
 
-let init () = { Color = "#00FF00" }, Cmd.none
+
+let startTick (ms: int) (handler: unit -> unit): TickId = Some <| window.setInterval (handler, ms)
+
+let stopTick (id: TickId) =
+    match id with
+    | Some n -> window.clearInterval (n)
+    | _ -> ()
+
+let startTicking dispatch =
+    let tickId = startTick 40 (fun _ -> Tick |> dispatch)
+    dispatch (SetTickId tickId)
+
+let stopTicking id dispatch =
+    stopTick id
+    dispatch (SetTickId None)
+
+let init () =
+    { Color = "#00FF00"; Tick = None }, Cmd.ofSub startTicking
 
 let update msg model =
     match msg with
-    | SetColor c -> { model with Color = c }, Cmd.none
-
+    | SetColor c -> { model with Color = c }, Cmd.ofSub (stopTicking model.Tick)
+    | SetTickId id -> { model with Tick = id }, Cmd.none
+    | Tick ->
+        { model with
+              Color =
+                  model.Color
+                  |> ColorShift.hexToHsv
+                  |> (ColorShift.rotateHue -0.005)
+                  |> ColorShift.hsvToHex },
+        Cmd.none
 
 // See original version of this demo for Elm here:
 // https://github.com/mrdimosthenis/turtle-graphics/blob/master/examples/SquareSpiral.elm
@@ -33,8 +61,7 @@ let rec iter n dist =
         rotateHue 0.002
         increaseWidth 0.02
         increaseAlpha -0.005
-        ifThen (n > 0)
-            (iter (n-1) (dist + 2.5))
+        ifThen (n > 0) (iter (n - 1) (dist + 2.5))
     }
 
 let drawTurtle color =
@@ -56,23 +83,30 @@ let view model dispatch =
                   MarginTop "48px" ] ] [
         Heading.h5 [] [
             str "Turtle Graphics with DrawingCanvas"
-            ]
+        ]
         drawingcanvas
-            { Props = [ Style [ Background "#333333"; Width 500; Height 500 ] ]
+            { Props =
+                  [ Style [ Background "#333333"
+                            Width 500
+                            Height 500 ] ]
               Redraw = model.Color |> drawTurtle |> Drawing }
-        Label.label [] [ str "Select base colour" ]
-        Field.div [ ] [
-            Input.color [
-                Input.Value (model.Color)
-                Input.Props [ OnChange (fun e -> e.Value |> SetColor |> dispatch) ]
-            ]
+        Label.label [] [
+            str "Select base colour"
+        ]
+        Field.div [] [
+            Input.color [ Input.Value(model.Color)
+                          Input.Props [ OnChange(fun e -> e.Value |> SetColor |> dispatch) ] ]
         ]
         Text.p [ Props [ Style [ FontSize "60%" ] ] ] [
             str "Built with "
-            a  [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas" ] [str "Fable.React.DrawingCanvas" ]
+            a [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas" ] [
+                str "Fable.React.DrawingCanvas"
+            ]
         ]
         Text.p [ Props [ Style [ FontSize "60%" ] ] ] [
-            a  [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas/tree/main/demos/Turtle/Turtle.fs" ] [str "Source" ]
+            a [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas/tree/main/demos/Turtle/Turtle.fs" ] [
+                str "Source"
+            ]
         ]
     ]
 
