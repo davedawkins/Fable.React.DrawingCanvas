ecd55a3c60bfedf74c73d681b0b20e35bc1b389d
David Dawkins <david.dawkins@gmail.com>
2020-12-13 10:51:05 +0000

    Performance improvements - only collide bubbles of same color

--- demos/Bubbles/Bubbles.fs
+++ demos/Bubbles/Bubbles.fs
@@ -13,7 +13,7 @@ open Browser.Dom
 // https://davidwalsh.name/canvas-demos
 // https://codepen.io/eltonkamami/pen/ECrKd
 
-let particlesNum = 300
+let particlesNum = 400
 let            w = 500.0
 let            h = 500.0
 let       colors = ["#f35d4f"; "#f36849"; "#c0d988"; "#6ddaf1"; "#f1e85b"]
@@ -21,7 +21,7 @@ let       colors = ["#f35d4f"; "#f36849"; "#c0d988"; "#6ddaf1"; "#f1e85b"]
 let rnd = System.Random()
 let random() = rnd.NextDouble()
 
-type Factory = {
+type Bubble = {
     mutable x : float
     mutable y : float
     rad : float
@@ -30,7 +30,7 @@ type Factory = {
     vy : float
 }
 
-let makeFactory() = {
+let makeBubble() = {
     x  = round( random() * w)
     y  = round(  random() * h)
     rad = round( random() * 1.0) + 1.0
@@ -39,10 +39,15 @@ let makeFactory() = {
     vy = round( random() * 3.0) - 1.5
 }
 
-let particles = [| 1..particlesNum |] |> Array.map (fun _ -> makeFactory())
 
+let allBubbles = [ 1..particlesNum ] |> List.map (fun _ -> makeBubble())
+
+// We only need to collide bubbles of the same color, so create list-per-color
+let bubblesByColor = colors |> List.map (fun c -> allBubbles |> List.filter (fun b -> b.rgba = c))
+
+// Avoid taking the sqrt
 let sq x = x * x
-let findDistanceSq (p1 : Factory) (p2:Factory) =
+let findDistanceSq (p1 : Bubble) (p2:Bubble) =
     ( sq(p2.x - p1.x)  + sq(p2.y - p1.y) )
 
 let draw (ctx : CanvasRenderingContext2D) =
@@ -50,45 +55,37 @@ let draw (ctx : CanvasRenderingContext2D) =
     ctx.canvas.height <- ctx.canvas.offsetHeight
     ctx.clearRect(0.0, 0.0, w, h)
     ctx.globalCompositeOperation <- "lighter"
+    ctx.lineWidth <- 0.5
 
-    for i in [0..(particlesNum-1)] do
-        let temp = particles.[i]
-        let mutable factor = 1.0
+    for bubbles in bubblesByColor do
+        // Only collide bubbles of the same color
+        for b1 in bubbles do
+            let mutable factor = 1.0
 
-        for j in [0..(particlesNum-1)] do
+            for b2 in bubbles do
+               if ((findDistanceSq b1 b2) < 2500.0) then
+                  ctx.strokeStyle <- U3.Case1 b1.rgba
+                  ctx.beginPath()
+                  ctx.moveTo(b1.x, b1.y)
+                  ctx.lineTo(b2.x, b2.y)
+                  ctx.stroke()
+                  factor <- factor + 1.0
 
-           let temp2 = particles.[j]
-           ctx.lineWidth <- 0.5
+            ctx.fillStyle <- U3.Case1 b1.rgba
+            ctx.strokeStyle <- U3.Case1 b1.rgba
 
-           if (temp.rgba = temp2.rgba && (findDistanceSq temp temp2) < 2500.0) then
-              ctx.strokeStyle <- U3.Case1 temp.rgba
-              ctx.beginPath()
-              ctx.moveTo(temp.x, temp.y)
-              ctx.lineTo(temp2.x, temp2.y)
-              ctx.stroke()
-              factor <- factor + 1.0
+            ctx.beginPath()
+            ctx.arc(b1.x, b1.y, b1.rad*factor, 0.0, Math.PI*2.0, true)
+            ctx.fill()
+            ctx.closePath()
 
-        ctx.fillStyle <- U3.Case1 temp.rgba
-        ctx.strokeStyle <- U3.Case1 temp.rgba
+            ctx.beginPath();
+            ctx.arc(b1.x, b1.y, (b1.rad+5.0)*factor, 0.0, Math.PI*2.0, true)
+            ctx.stroke();
+            ctx.closePath();
 
-        ctx.beginPath()
-        ctx.arc(temp.x, temp.y, temp.rad*factor, 0.0, Math.PI*2.0, true)
-        ctx.fill()
-        ctx.closePath()
-
-        ctx.beginPath();
-        ctx.arc(temp.x, temp.y, (temp.rad+5.0)*factor, 0.0, Math.PI*2.0, true)
-        ctx.stroke();
-        ctx.closePath();
-
-        temp.x <- temp.x + temp.vx
-        temp.y <- temp.y + temp.vy
-
-        if (temp.x > w) then temp.x <- 0.0
-        if (temp.x < 0.0) then temp.x <- w
-        if (temp.y > h) then temp.y <- 0.0
-        if (temp.y < 0.0) then temp.y <- h
-
+            b1.x <- ((b1.x + b1.vx) + w) % w
+            b1.y <- ((b1.y + b1.vy) + h) % h
 
 type Model = {
     Time : float
@@ -117,17 +114,22 @@ let view model dispatch =
                   Width "500px"
                   MarginTop "48px" ] ] [
         Heading.h5 [] [
-            str "More Particles"
+            str "Bubbles"
             ]
         drawingcanvas
             { Props = [ Style [ Background "black"; Width 500; Height 500 ] ]
               Redraw = DrawFunction draw }
         Text.p [ Props [ Style [ FontSize "60%" ] ] ] [
+            p [] [
+                str "Source: "
+                a  [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas/tree/main/demos/Bubbles/Bubbles.fs" ] [str "Bubbles.fs" ] ]
+        ]
+        Text.p [ Props [ Style [ FontSize "60%" ] ] ] [
             str "Built with "
             a  [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas" ] [str "Fable.React.DrawingCanvas" ]
         ]
-        Text.p [ Props [ Style [ FontSize "60%" ] ] ] [
-            a  [ Href "https://github.com/davedawkins/Fable.React.DrawingCanvas/tree/main/demos/Fractal/Fractal.fs" ] [str "Source" ]
+        Text.p [ Props [ Style [ FontSize "60%"; MarginTop 4 ] ] ] [
+            a  [ Href "https://codepen.io/eltonkamami/pen/ECrKd" ] [str "Original by Elton Kamami" ]
         ]
     ]
 
